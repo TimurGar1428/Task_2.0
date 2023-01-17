0622a43048b089f91fcafe7ecfa8be82845aadbc
TimurGar1428 <122256050+TimurGar1428@users.noreply.github.com>
2023-01-17 12:56:22 +0300

    Черновик

---
+++ SteamAutorization_Test/PageObjects/MainMenyPageObject.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamAutorization_Test.PageObjects
+{
+    internal class MainMenyPageObject
+    {
+        private IWebDriver _webDriver;
+        private readonly By _signInButton =
+            By.XPath("//*[@id=\"global_action_menu\"]/a");
+        private readonly By _confPolitic =
+            By.XPath("//*[@id=\"footer_text\"]/div[2]/a[1]");
+
+        public MainMenyPageObject(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+        /// 
+        public AutorizationPageObjects SignIn()
+        {
+            _webDriver.FindElement(_signInButton).Click();
+
+            return new AutorizationPageObjects(_webDriver);
+        }
+       
+        ///
+        public ConfPoliticPageObject openConfPolit()
+        {
+            _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            _webDriver.FindElement(_confPolitic).Click();
+            return new ConfPoliticPageObject(_webDriver);
+
+        }
+    }
+}
